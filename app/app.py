2c9277d223ec6699f651cc38d93a40ff694e93f3
NinerMG <maciej.gala@hotmail.com>
2025-11-06 17:25:05 +0100

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+MY_PROJECTS = [
+    {
+        "name": "Morse Code Converter (Architektura & QA)",
+        "description": "Wielomodułowa aplikacja (architektura MVC) z pełnym pokryciem testowym (Pytest). Udowadnia moją dbałość o jakość kodu, obsługę przypadków brzegowych i umiejętności inżynierskie (np. generowanie audio w NumPy).",
+        "link": "https://github.com/NinerMG/MorseCodeConverter",
+        "tags": ["OOP", "Tkinter", "Pytest", "NumPy", "QA"],
+        "img_url": "app/static/images/projects/morse_code_generator.png"
+    }
+]
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', projects=MY_PROJECTS)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
